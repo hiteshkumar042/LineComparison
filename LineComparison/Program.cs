6e8e2709f5ea9c029d367030b797c1bdcb600e27
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-19 23:25:47 +0530

    Modifying program.cs to get LengthOfLine

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -5,6 +5,19 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problem!");
+            //Length of Line - UC1
+            Console.WriteLine("Enter X1 co-ordinate : ");
+            double x1=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y1 co-ordinate : ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter X2 co-ordinate : ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y2 co-ordinate : ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+            Console.WriteLine("length of teh Line is : " + length);
         }
     }
 }
